4865d621b5e94074d0fe46b6603d7d98ab123374
Astronot233 <tyz.err.233@gmail.com>
2026-08-22 12:18:28 +0800

    Axolotl 1.8.10

--- package.nix
+++ package.nix
@@ -6,18 +6,18 @@
 }:
 stdenv.mkDerivation rec {
   pname = "axolotl";
-  version = "1.8.9";
+  version = "1.8.10";
   src = (
     let
       base = "https://github.com/Mystic-Stars/Axolotl/releases/download/v${version}";
       debs = {
         x86_64-linux = {
           url = "${base}/Axolotl.Launcher_${version}_amd64.deb";
-          hash = "sha256:33bd70467b286e13174bc707244a9a922b953397af97f96178e7214290d628ba";
+          hash = "sha256:607d8241cd6fcfb304acc7b77a388b6039e267278da129876e25792f615b89df";
         };
         aarch64-linux = {
           url = "${base}/Axolotl.Launcher_${version}_arm64.deb";
-          hash = "sha256:69f4537b3b25f9146184bcebbe8d07e13abb3ffae3a589344294bef8d7a139d6";
+          hash = "sha256:14076014cf6d442d484396b87f0fad4d73cd1043395fbc43654df35def6bb579";
         };
       };
       sys = stdenv.hostPlatform.system;
